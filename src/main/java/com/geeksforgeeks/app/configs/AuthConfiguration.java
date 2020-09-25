fb8e730a1f294b6e5722baf0cc3be7ccc8fa2115
Dharmendra Kumar Awasthi <awadha01@CSGICORP.COM>
2020-09-25 20:00:25 +0530

    Adding security

---
+++ src/main/java/com/geeksforgeeks/app/configs/AuthConfiguration.java
@@ -0,0 +1,12 @@
+package com.geeksforgeeks.app.configs;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthConfiguration implements WebMvcRegistrations{
+	
+	public void addViewControllers() {
+		
+	}
+}
